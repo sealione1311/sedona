21899fe2561a808e4366cfbe0d608847b3244177
sealione1311 <sealione@gmail.com>
2019-05-25 22:47:59 +0300

    Исправила ошибки и переформатировала картинки в webp

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,13 @@ var csso = require("gulp-csso");
 var server = require("browser-sync").create();
 var del = require("del");
 var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp")
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+});
 
 gulp.task("images", function () {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
